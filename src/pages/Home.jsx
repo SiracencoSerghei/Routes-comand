e789f21327cfc80aa3cf0040e40245ddfba3b724
Siracenco Serghei <siracencoserghei@gmail.com>
2023-06-01 20:11:20 +0200

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useState, useEffect } from 'react';
+import { getCountries } from 'service/country-service';
+
+
+export const Home = () => {
+
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(()=>{
+    const fetchCountries = async () => {
+      try {
+        setIsLoading(true);
+       const result = await getCountries();
+       setCountries(result);
+       console.log('result', result)
+      } catch (error) {
+        setError(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCountries()
+  },[])
+
+  return (
+    <Section>
+      <Container>
+        {isLoading && <Loader />}
+        {error && <Heading>Oops, something wrong</Heading>}
+        <CountryList countries={countries} />
+      </Container>
+    </Section>
+  );
+};
