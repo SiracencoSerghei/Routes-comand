9cbd289aca9198b880288c304f36d42d8aac68aa
Siracenco Serghei <siracencoserghei@gmail.com>
2023-06-03 11:26:25 +0200

    styles btn

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -10,6 +10,15 @@ export const Country = () => {
   const [country, setCountry] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const backBtnStyle = {
+    backgroundColor: 'white',
+    color: '#22a6b3',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    padding: '4px',
+    margin: '4px auto',
+    textAline: 'center',
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -45,7 +54,9 @@ export const Country = () => {
           languages={languages}
           population={population}
         />
-        <Link to={backLink}>Повернутися назад</Link>
+        <Link to={backLink}>
+        <button style={backBtnStyle}>Go BACK</button>
+      </Link>
       </Container>
     </Section>
   );
